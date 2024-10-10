7467f7c77619dd9dd024efabc1168e14707a9d7a
DanielJimenez0429 <danieljimenez200429@gmail.com>
2024-10-09 22:48:39 -0500

    Se creo un record de Video con sus atributos en el paquete model y se creo tambien la interface VideoRepository en el paquete repository

---
+++ src/main/java/culturemedia/repository/VideoRepository.java
@@ -0,0 +1,12 @@
+package culturemedia.repository;
+
+import java.util.List;
+
+import culturemedia.model.Video;
+
+public interface VideoRepository {
+    List<Video> listarTodos();
+    Video agregar(Video video);
+    List<Video> buscar(String titulo);
+    List<Video> buscar(Double duracionDesde, Double duracionHasta);
+}
